0fed72bfbd2cb64ade0979dd8a0140a7aa554667
JessicaSung <jessica.sung@utexas.edu>
2016-09-14 14:04:13 -0500

    updated js with code we worked on in 7.3 class

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,4 +8,72 @@
   };
   firebase.initializeApp(config);
 
+
+  // VARIABLES
+	var database = firebase.database();
+
+	var trainName = "";
+	var destination = "";
+	var firstTrainTime = 0;
+	var frequency = 0;
+
+
+// FUNCTIONS + EVENTS
+$("addTrain").on("click", function() {
+
+  trainName = $('#nameInput').val().trim();
+  destination = $('#destinationInput').val().trim();
+  firstTrainTime = $('#firstTrainInput').val().trim();
+  frequency = $('#frequencyInput').val().trim();
+
+  console.log(trainName);
+  console.log(destination);
+  console.log(firstTrainTime);
+  console.log(frequency);
+
+  database.ref().push({
+    trainName: trainName,
+    destination: destination,
+    firstTrainTime: firstTrainTime,
+    frequency: frequency
+  });
+
+    return false;
+});
+
+
+// MAIN PROCESS + INITIAL CODE
+  database.ref().on("child_added", function(snapshot) {
+    console.log(snapshot.val());
+
+    // update the variable with data from the database
+    trainName = snapshot.val().trainName;
+    destination = snapshot.val().destination;
+    firstTrainTime = snapshot.val().firstTrainTime;
+    frequency = snapshot.val().frequency;
+
+    // add table
+    var tr = $('<tr>');
+    var a = $('<td>');
+    var b = $('<td>');
+    var c = $('<td>');
+    var d = $('<td>');
+    var e = $('<td>');
+    a.append(trainName);
+    b.append(destination);
+    c.append(frequency);
+    d.append("Next Arrival");
+    e.append("Minutes Away");
+    tr.append(a).append(b).append(c).append(d).append(e);
+    $('#newTrains').append(tr);
+
+
+    }, function (errorObject) {
+
+    // In case of error this will print the error
+      console.log("The read failed: " + errorObject.code);
   
+  });
+
+
+
