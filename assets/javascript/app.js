e948a47aff15aec2174a9d1e8defde99421e0fa0
JessicaSung <jessica.sung@utexas.edu>
2016-09-13 22:05:59 -0500

    scrapped the html and copied the layout we built in the in-class activity

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBmRQGGDRAYPh_j5_urgHgQhnS80NVHils",
+    authDomain: "train-time-70216.firebaseapp.com",
+    databaseURL: "https://train-time-70216.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "919046156008"
+  };
+  firebase.initializeApp(config);
+
+  
