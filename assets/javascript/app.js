90407fb5d502ddaa9f612d7832dd33ff72a938b9
JessicaSung <jessica.sung@utexas.edu>
2016-11-14 11:15:34 -0600

    Adjusted indentation to remove whitespace for a cleaner look.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,21 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBmRQGGDRAYPh_j5_urgHgQhnS80NVHils",
-    authDomain: "train-time-70216.firebaseapp.com",
-    databaseURL: "https://train-time-70216.firebaseio.com",
-    storageBucket: "",
-    messagingSenderId: "919046156008"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyBmRQGGDRAYPh_j5_urgHgQhnS80NVHils",
+  authDomain: "train-time-70216.firebaseapp.com",
+  databaseURL: "https://train-time-70216.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "919046156008"
+};
+firebase.initializeApp(config);
 
 
-  // VARIABLES
-	var database = firebase.database();
+// VARIABLES
+var database = firebase.database();
 
-	var trainName = "";
-	var destination = "";
-	var firstTrainTime = "";
-	var frequency = 0;
+var trainName = "";
+var destination = "";
+var firstTrainTime = "";
+var frequency = 0;
 
 
 // FUNCTIONS + EVENTS
@@ -43,48 +43,48 @@ $("#addTrain").on("click", function() {
 
 
 // MAIN PROCESS + INITIAL CODE
-  database.ref().on("child_added", function(snapshot) {
-    console.log(snapshot.val());
+database.ref().on("child_added", function(snapshot) {
+  console.log(snapshot.val());
 
-    // update the variable with data from the database
-    trainName = snapshot.val().trainName;
-    destination = snapshot.val().destination;
-    firstTrainTime = snapshot.val().firstTrainTime;
-    frequency = snapshot.val().frequency;
-
-
-    // moment.js methods for time calls and calculations. lines 57 to 65 were accomplished with Tenor's assistance. I didn't update the current time. It looks like "Minutes Away" may be larger than the frequency interval :(
-    var firstTrainMoment = moment(firstTrainTime, 'HH:mm');
-    var nowMoment = moment(); // creates a moment object of current date and time and storing it in a variable whenever the user click the submit button
-
-    var minutesSinceFirstArrival = nowMoment.diff(firstTrainMoment, 'minutes');
-    var minutesSinceLastArrival = minutesSinceFirstArrival % frequency;
-    var minutesAway = frequency - minutesSinceLastArrival;
-  
-    var nextArrival = nowMoment.add(minutesAway, 'minutes');
-    var formatNextArrival = nextArrival.format("HH:mm");
+  // update the variable with data from the database
+  trainName = snapshot.val().trainName;
+  destination = snapshot.val().destination;
+  firstTrainTime = snapshot.val().firstTrainTime;
+  frequency = snapshot.val().frequency;
 
 
-    // add table
-    var tr = $('<tr>');
-    var a = $('<td>');
-    var b = $('<td>');
-    var c = $('<td>');
-    var d = $('<td>');
-    var e = $('<td>');
-    a.append(trainName);
-    b.append(destination);
-    c.append(frequency);
-    d.append(formatNextArrival);
-    e.append(minutesAway);
-    tr.append(a).append(b).append(c).append(d).append(e);
-    $('#newTrains').append(tr);
+  // moment.js methods for time calls and calculations. lines 57 to 65 were accomplished with Tenor's assistance. I didn't update the current time. It looks like "Minutes Away" may be larger than the frequency interval :(
+  var firstTrainMoment = moment(firstTrainTime, 'HH:mm');
+  var nowMoment = moment(); // creates a moment object of current date and time and storing it in a variable whenever the user click the submit button
+
+  var minutesSinceFirstArrival = nowMoment.diff(firstTrainMoment, 'minutes');
+  var minutesSinceLastArrival = minutesSinceFirstArrival % frequency;
+  var minutesAway = frequency - minutesSinceLastArrival;
+
+  var nextArrival = nowMoment.add(minutesAway, 'minutes');
+  var formatNextArrival = nextArrival.format("HH:mm");
 
 
-    }, function (errorObject) {
+  // add table
+  var tr = $('<tr>');
+  var a = $('<td>');
+  var b = $('<td>');
+  var c = $('<td>');
+  var d = $('<td>');
+  var e = $('<td>');
+  a.append(trainName);
+  b.append(destination);
+  c.append(frequency);
+  d.append(formatNextArrival);
+  e.append(minutesAway);
+  tr.append(a).append(b).append(c).append(d).append(e);
+  $('#newTrains').append(tr);
 
-    // In case of error this will print the error
-      console.log("The read failed: " + errorObject.code);
-  
-  });
+
+  }, function (errorObject) {
+
+  // In case of error this will print the error
+    console.log("The read failed: " + errorObject.code);
+
+});
 
